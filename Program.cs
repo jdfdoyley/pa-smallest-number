711d76f77fffe54e51f917507f41e4034cf02045
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-12 05:34:35 -0400

    Find and print smallest input value

--- Program.cs
+++ Program.cs
@@ -15,8 +15,39 @@ public class LoopCalculation
 {
     static void Main(string[] args)
     {
-        // Print a header line
+        // Print a line that states "Your Name - Week 2 PA Smallest Number"
         Console.WriteLine("\nJason D'Oyley - Week 2 PA Smallest Number\n");
 
+        // Ask the user how many integers they will enter
+        Console.WriteLine("Finding the Smallest Value:");
+        Console.Write("How many integers would you like to enter: ");
+        string? val = Console.ReadLine();
+        int inVal = Convert.ToInt32(val);
+
+        int inVal2 = 0;
+        int smallestVal = 0;
+
+        // Loop to get the specified number of integers from the user
+        for (int i = 0; i < inVal; i++)
+        {
+            Console.Write("Enter an integer value: ");
+            val = Console.ReadLine();
+            inVal2 = Convert.ToInt32(val);
+
+            if (i == 0)
+            {
+                smallestVal = inVal2;
+                continue;
+            }
+
+            if (inVal2 < smallestVal)
+            {
+                smallestVal = inVal2;
+            }
+
+        }
+
+        // Print the value of the smallest integer entered by the user
+        Console.WriteLine("The smallest value entered is: {0}", smallestVal);
     }
 }
