d5801556540c6b32e24d014b18ec6d36aeeec212
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-12 05:18:49 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/******************************************************************************\
+* Name: Jason D. F. D'Oyley
+* Date: Oct. 12, 2022
+* Assignment CIS214 Performance Assessment - Smallest Number
+*
+* Main application (program) class.
+* The main application class must meet the following requirements
+* - Print a line that states "Your Name - Week 2 PA Smallest Number"
+* - Ask the user how many integers they will enter
+* - Loop to get the specified number of integers from the user
+* - Print the value of the smallest integer entered by the user
+*
+\******************************************************************************/
+public class LoopCalculation
+{
+    static void Main(string[] args)
+    {
+        // Print a header line
+        Console.WriteLine("\nJason D'Oyley - Week 2 PA Smallest Number\n");
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Smallest Number
+
+The main application class must meet the following requirements
+
+- Print a line that states "Your Name - Week 2 PA Smallest Number"
+- Ask the user how many integers they will enter
+- Loop to get the specified number of integers from the user
+- Print the value of the smallest integer entered by the user
